5c4c8f91035d9f6a0dfabc444568cdcd8d060cb7
p15co <arevollar2310@gmail.com>
2022-08-24 16:01:27 +1000

    90 percent

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ConsolePrint : MonoBehaviour
+public class PrintAndHide : MonoBehaviour
 {
     public GameObject gameObject;
-
+    public Renderer rend;
     private int i = 3;
 
 
